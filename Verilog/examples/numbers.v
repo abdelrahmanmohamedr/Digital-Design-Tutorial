0fb6b12ae401a425a9b60829611013fd3651fc23
RawanAbdelkader272 <rawan.abdelkader2722@gmail.com>
2026-08-12 06:35:46 +0300

    Delete Verilog/examples directory

--- Verilog/examples/numbers.v
+++
@@ -1,7 +0,0 @@
-// Basic module syntax with port list
-module <name> ([port_list]);
-	// Variable declarations (wires, regs, integers, etc.)
-	// Dataflow statements (assign, always, initial blocks)
-	// Function and task definitions
-	// Sub-module instantiations
-endmodule
