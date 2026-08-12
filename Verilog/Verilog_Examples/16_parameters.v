19903c0943a11b95ea6de6cce08c66ac3cb35670
abdelrahmanmohamedr <139080958+abdelrahmanmohamedr@users.noreply.github.com>
2026-08-12 14:16:57 +0300

    Update 16_parameters.v

--- Verilog/Verilog_Examples/16_parameters.v
+++ Verilog/Verilog_Examples/16_parameters.v
@@ -12,4 +12,4 @@ parameter REAL = 4.5; // REAL holds a real number
 parameter FIFO_DEPTH = 256,
  MAX_WIDTH = 32; // Declares two parameters
 
-parameter [7:0] f_const = 2'b3; // 2 bit value is converted to 8 bits; 8'b3
+    parameter [7:0] f_const = 2'b11; // 2 bit value is converted to 8 bits; 8'b11
