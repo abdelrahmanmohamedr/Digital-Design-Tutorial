4a53886a83abb62e2303a9a62ff478495f1741c8
abdelrahmanmohamedr <139080958+abdelrahmanmohamedr@users.noreply.github.com>
2026-08-12 14:20:16 +0300

    Update 17_parameters_override_instantiation.v

--- Verilog/Verilog_Examples/17_parameters_override_instantiation.v
+++ Verilog/Verilog_Examples/17_parameters_override_instantiation.v
@@ -4,9 +4,9 @@ counter #(.WIDTH(16)) big_counter (.count(count_out));
 module tb;
 
     // Module instantiation override
-        design_ip #(BUS_WIDTH = 64, DATA_WIDTH = 128) d0 ( [port list]);
+    design_ip #(.BUS_WIDTH(64), .DATA_WIDTH(128)) d0 ( [port list]);
 
-        // Use of defparam to override
-        defparam d0.FIFO_DEPTH = 128; //commonly used in testbench simulations to quickly update the design parameters without having to reinstantiate the module.
+    // Use of defparam to override
+    defparam d0.FIFO_DEPTH = 128; //commonly used in testbench simulations to quickly update the design parameters without having to reinstantiate the module.
 
 endmodule
